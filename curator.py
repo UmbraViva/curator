00070e1978ff2d05818630d1165fb81b4013fc71
UmbraViva <106489425+UmbraViva@users.noreply.github.com>
2025-07-14 12:58:19 -0500

    feat: puts rest of document in "Notes" field

--- curator.py
+++ curator.py
@@ -18,15 +18,24 @@ def extract_metadata(pdf_path: str) -> Dict[str, str]:
         "Dimensions:": "Dimensions",
     }
 
+    notes: List[str] = []
+
     try:
         with pdfplumber.open(pdf_path) as pdf:
             for page in pdf.pages:
                 page_text: List[str] = page.extract_text().splitlines() 
+
                 if page_text:  
                     for line in page_text:
+                        if line == "Kurzgefasste Liste description:" or line == "Corrections to K–Liste description:":
+                            continue
+
                         for search_term, key in search_terms.items():
                             if search_term in line:
                                 metadata[key] = line.split(search_term, 1)[1].strip()
+                                break
+                        else:
+                            notes.append(line)
 
     except FileNotFoundError:
         print(f"Error: PDF file not found at {pdf_path}")
@@ -34,7 +43,8 @@ def extract_metadata(pdf_path: str) -> Dict[str, str]:
     except Exception as e:
         print(f"An error occurred: {e}")
         return {}
-
+    
+    metadata["Notes"] = "\n".join(notes)
     return metadata
 
 
